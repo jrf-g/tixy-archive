3e81606cb7a88143060578a707fdcb0ec163a556
jrf-g <jrfaragalla@gmail.com>
2026-09-02 14:55:24 -0700

    Add three new (t,i,x,y) functions to index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+const rain /*      */ = (t,i,x,y) => Math.tan((t * 5) - i); //-----|
+const wipe /*      */ = (t,i,x,y) => Math.abs(Math.sin(t + y)); //-|
+const glitch_inchworm = (t,i,x,y) => Math.tan(t - y); //-----------|
