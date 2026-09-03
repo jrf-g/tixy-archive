2b9f960068f871a80495ab508da7b29fbce33e0f
jrf-g <jrfaragalla@gmail.com>
2026-09-02 19:19:08 -0700

    Refactor functions in index.js with wiggle addition

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-const rain /*      */ = (t,i,x,y) => Math.tan((t * 5) - i); //-----|
-const wipe /*      */ = (t,i,x,y) => Math.abs(Math.sin(t + y)); //-|
-const glitch_inchworm = (t,i,x,y) => Math.tan(t - y); //-----------|
+const rain /*            */ = (t,i,x,y) => Math.tan((t * 5) - i); //-----------|
+const wiggle /*          */ = (t,i,x,y) => Math.tan(Math.sin(t * 30) - i); // -|
+const wipe /*            */ = (t,i,x,y) => Math.abs(Math.sin(t + y)); //-------|
+const glitch_inchworm /* */ = (t,i,x,y) => Math.tan(t - y); //-----------------|
